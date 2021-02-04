8f065bca9b05357f29da4e005e63934da444a8f9
Victor de Oliveira <oliveira.wn@gmail.com>
2021-02-03 21:21:31 -0300

    endpoints da api rest

--- routes/products.js
+++ routes/products.js
@@ -5,8 +5,10 @@ const productsController = require('../controllers/productsController')
 router.delete('/:id', productsController.remove)
 router.patch('/:id', productsController.patch)
 router.put('/:id', productsController.put)
-router.post('/', productsController.post)
-router.get('/:id', productsController.getProductById)
+router.post('/', productsController.create)
+router.get('/:id', productsController.getById)
 router.get('/', productsController.getAll)
+router.post('/:id/images', productsController.createImage)
+router.delete('/:productId/images/:id', productsController.removeImage)
 
 module.exports = router
